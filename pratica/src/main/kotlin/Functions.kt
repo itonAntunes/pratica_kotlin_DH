8ffc1e1251d0bc4f595215e5e9f69b8abe3c2e27
CelsoMatiazi <celsomatiazi@gmail.com>
2021-07-30 21:59:01 -0300

    primeira pratica com kotlin

---
+++ pratica/src/main/kotlin/Functions.kt
@@ -0,0 +1,13 @@
+fun main(){
+    print(soma(2,3))
+    semRetorno(23,12)
+}
+
+fun soma(numero1: Int, numero2: Int) : Int{
+    return numero1 + numero2
+}
+
+fun semRetorno(numero1: Int, numero2: Int){
+    print("\n")
+    print(numero1 - numero2)
+}
